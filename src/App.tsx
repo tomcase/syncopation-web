e29f0d3aa5f9ff684ecd78408f8ef9c19ba6b20d
Tom Case <tomcase77@gmail.com>
2021-04-25 17:50:02 +0100

    Add sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { ApiStatus } from './components/ApiStatus/ApiStatus';
 
 const App = () => {
+
+  const runSync = () => {
+    (async () => {
+      const url = process.env.REACT_APP_API_URL;
+      await fetch(`${url}/api/sync`, { method: "POST"});
+    })();
+  }
+
   return (
     <div className={`container mx-auto`}>
       <div className={`flex`}>
         <ApiStatus />
+        <button className={"px-4 py-2 w-24 rounded-lg bg-blue-500 text-gray-200 shadow-lg"} onClick={runSync}>Sync</button>
       </div>
     </div>
   );
